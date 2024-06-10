542b903173d76f661acb189924027f8d33cb01b3
pravinboopathi <pravinboopathi1100@gmail.com>
2024-06-10 20:40:55 +0530

    order change for members

--- src/components/ExecutiveDetails.jsx
+++ src/components/ExecutiveDetails.jsx
@@ -7,7 +7,6 @@ import Rishaba from '../images/Executive Mmebers/rishaba.webp'
 import Vishnuraj from '../images/Executive Mmebers/VISHNURAJ R.webp'
 import Aarthi from '../images/Executive Mmebers/aarthi.webp'
 import Velan from '../images/Executive Mmebers/velan.jpeg'
-import kanishka from '../images/Executive Mmebers/kanishka.webp'
 import logeshwaran from '../images/Executive Mmebers/logeshwaran.jpeg'
 import Nandita from '../images/Executive Mmebers/Nandita.jpg'
 import Sarath from '../images/Executive Mmebers/sarath.webp'
@@ -20,25 +19,26 @@ import Chittesh from '../images/Executive Mmebers/chittesh.webp'
 
 
 export const executiveDetails = [
-    { name: 'Velan J', img: Velan , dept: 'B.Sc CS DA - II', linkedin:'https://www.linkedin.com/in/velan-jayashankar', instagram:'https://www.instagram.com/ig__velan__' },
-    { name: 'Balaguru K', img: Balaguru , dept: 'B.Sc CS - III', linkedin:'https://www.linkedin.com/in/balaguru-k-22157727a?', },
-    { name: 'Sujithaa J', img: Sujithaa , dept: 'B.Sc CS with CS - II', linkedin:'https://www.linkedin.com/in/sujithaa-j-41643b292?' },
-    { name: 'Saratthi', img: SriSaratthi , dept: 'B.Sc IT - III', linkedin:'https://www.linkedin.com/in/saratthi-080555166' },
-    { name: 'Harini', img: harini , dept: 'B.Sc CS with CS - III', linkedin:'https://www.linkedin.com/in/harini-pechimuthu-1b47972b5',instagram:'https://www.instagram.com/harinipechimuthu?' }, 
-    { name: 'Rishaba R U', img: Rishaba , dept: 'B.Sc CS CS - III', linkedin :' https://www.linkedin.com/in/rishaba0305204?' },
-    { name: 'Vishnuraj R', img: Vishnuraj , dept: 'B.Sc CS CS - III', linkedin:'https://www.linkedin.com/in/vishnuraj-r-a3229324b/' , instagram:'https://www.instagram.com/vishnu._.rvr/' },
     { name: 'Aarthi S', img: Aarthi , dept: 'B.Sc CT - II', linkedin:'https://www.linkedin.com/in/aarthi-s-8914a9292?' , instagram:'https://www.instagram.com/__.aarthi___?' },
-    { name: 'Kanishka C', img: kanishka , dept: 'B.Sc CS (AI & DS) - II', linkedin:'https://www.linkedin.com/in/kaniskha-c-2124a5292?' , instagram:'https://www.instagram.com/kaniskha_chandrasekar?' },
+    { name: 'Balaguru K', img: Balaguru , dept: 'B.Sc CS - III', linkedin:'https://www.linkedin.com/in/balaguru-k-22157727a?', },
+    { name: 'Chittesh', img: Chittesh , dept: 'B.Sc CS CS', linkedin:'https://www.linkedin.com/in/chittesh-undefined-753499306?', instagram:'https://www.instagram.com/chittesh___23?' },
+    { name: 'Gokul S', img: Gokul , dept: 'B.Sc IT - III', linkedin:'https://www.linkedin.com/in/gokul-saravanan-s-74bb9327b?', instagram:'https://www.instagram.com/lil_dodge5?' },
+    { name: 'Harini', img: harini , dept: 'B.Sc CS with CS - III', linkedin:'https://www.linkedin.com/in/harini-pechimuthu-1b47972b5',instagram:'https://www.instagram.com/harinipechimuthu?' }, 
     { name: 'Logeshwaran V', img: logeshwaran , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/logesh-waran-a42395227?' , instagram:'https://www.instagram.com/aeksio__hen__vys?'},
     { name: 'Nandita', img: Nandita , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/nandita-s-a38948312?' },
-    { name: 'Sarath P', img: Sarath , dept: 'B.Sc IT - II', linkedin:'http://www.linkedin.com/in/thisisthesarath' , instagram:'http://www.instagram.com/sarath.online' },
-    { name: 'Gokul S', img: Gokul , dept: 'B.Sc IT - III', linkedin:'https://www.linkedin.com/in/gokul-saravanan-s-74bb9327b?', instagram:'https://www.instagram.com/lil_dodge5?' },
-    { name: 'Vikaas K S', img: Vikaas , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/vikaas-k-s-7644a9292?' ,instagram:'https://www.instagram.com/vikaas_0304?' },
-    { name: 'Poornila I P', img: Poornila , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/poornila-i-p-45b0b4292?', instagram:'https://www.instagram.com/click_.natural?' },
-    { name: 'Srikanth R G', img: srikanth , dept: 'B.Sc CS CS - II', linkedin:'https://www.linkedin.com/in/srikanth-r-g-544937283?' , instagram:'https://www.instagram.com/spetsnaz_srikanth?' },
     { name: 'Priyadharshini K', img: PriyaDharshini , dept: 'B.Sc CS CS - II', linkedin:'https://www.linkedin.com/in/priyadharshini-k-784920275'},
-    { name: 'Chittesh', img: Chittesh , dept: 'B.Sc CS CS', linkedin:'https://www.linkedin.com/in/chittesh-undefined-753499306?', instagram:'https://www.instagram.com/chittesh___23?' },
+    { name: 'Poornila I P', img: Poornila , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/poornila-i-p-45b0b4292?', instagram:'https://www.instagram.com/click_.natural?' },
+    { name: 'Rishaba R U', img: Rishaba , dept: 'B.Sc CS CS - III', linkedin :' https://www.linkedin.com/in/rishaba0305204?' },
+    { name: 'Sarath P', img: Sarath , dept: 'B.Sc IT - II', linkedin:'http://www.linkedin.com/in/thisisthesarath' , instagram:'http://www.instagram.com/sarath.online' },
+    { name: 'Saratthi', img: SriSaratthi , dept: 'B.Sc IT - III', linkedin:'https://www.linkedin.com/in/saratthi-080555166' },
+    { name: 'Srikanth R G', img: srikanth , dept: 'B.Sc CS CS - II', linkedin:'https://www.linkedin.com/in/srikanth-r-g-544937283?' , instagram:'https://www.instagram.com/spetsnaz_srikanth?' },
+    { name: 'Sujithaa J', img: Sujithaa , dept: 'B.Sc CS with CS - II', linkedin:'https://www.linkedin.com/in/sujithaa-j-41643b292?' },
+    { name: 'Velan J', img: Velan , dept: 'B.Sc CS DA - II', linkedin:'https://www.linkedin.com/in/velan-jayashankar', instagram:'https://www.instagram.com/ig__velan__' },
+    { name: 'Vikaas K S', img: Vikaas , dept: 'B.Sc IT - II', linkedin:'https://www.linkedin.com/in/vikaas-k-s-7644a9292?' ,instagram:'https://www.instagram.com/vikaas_0304?' },
+    { name: 'Vishnuraj R', img: Vishnuraj , dept: 'B.Sc CS CS - III', linkedin:'https://www.linkedin.com/in/vishnuraj-r-a3229324b/' , instagram:'https://www.instagram.com/vishnu._.rvr/' },
+    
+    
     // { name: 'Name', img: sampleImg , dept: 'B.Sc CS', },
-    // { name: 'Name', img: sampleImg , dept: 'B.Sc CS', },
+    // { name: 'Name', img: sampleImg , dept: 'B.Sc CS', }, 
    
 ]
