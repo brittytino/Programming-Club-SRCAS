ff6c35314ee3f024f0a52177a1cdf0a075b4fe9c
Pravin Boopathi <pravinboopathi396@gmail.com>
2024-06-10 10:15:10 +0530

    Update Co-ordinates Details.jsx

--- src/components/Co-ordinates Details.jsx
+++ src/components/Co-ordinates Details.jsx
@@ -27,7 +27,7 @@ export const staffCoordinators = [
 
 export const officeBearers = [
     { role: 'Chairman', name: 'Tino Britty J', img: chairman, dept: 'BSc CS',  instagram:'https://www.instagram.com/brittytino/' , linkedin:'https://www.linkedin.com/in/brittytino'},
-    { role: 'Assistant Chairman', name: 'John Samuel', img: assistantChairman, dept: 'BSc IT', instagram:'https://www.instagram.com/samjohn_offl' , linkedin:'https://www.linkedin.com/in/samjohnoffl' },
+    { role: 'Vice Chairman', name: 'John Samuel', img: assistantChairman, dept: 'BSc IT', instagram:'https://www.instagram.com/samjohn_offl' , linkedin:'https://www.linkedin.com/in/samjohnoffl' },
     { role: 'Secretary', name: 'Pravin B', img: secretary, dept:'BSc CS & DA', instagram : 'https://www.instagram.com/bpravin._/' , linkedin : 'https://www.linkedin.com/in/pravinboopathi/'  },
     { role: 'Joint Secretary', name: 'Kanishka C', img: viceChairman, dept:'BSc CS (AI & DS)', instagram:'https://www.instagram.com/kaniskha_chandrasekar/' , linkedin: 'https://www.linkedin.com/in/kaniskha-c-2124a5292/'},
     
