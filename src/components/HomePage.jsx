b38faa89d023dbecedae43424e55a6da5eaec40c
pravinboopathi <pravinboopathi1100@gmail.com>
2024-06-12 20:54:47 +0530

    btn size

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -22,12 +22,12 @@ const HomePage = () => {
                         where innovation and collaboration drive success.
                     </p>
                     <div className="flex items-center justify-center gap-5 mt-5 md:mt-10">
-                       <a href="https://forms.gle/hb8b8uyfHMSfY8hv6" target='blank'><button  className="hover:bg-[#3CB371] bg-[#28A745] font-semibold text-white transition-all text-2xl font-poppins md:text-4xl rounded-xl py-3 px-5">Join Now</button></a>
+                       <a href="https://forms.gle/hb8b8uyfHMSfY8hv6" target='blank'><button  className="hover:bg-[#3CB371] bg-[#28A745] font-semibold text-white transition-all text-xl font-poppins md:text-4xl rounded-xl py-3 px-5">Join Now</button></a>
                         {/* <button className="p-4 rounded-2xl text-[#28A745] text-xl md:text-2xl py-3 px-5 border border-[#28A745] hover:text-white hover:bg-[#28A745] transition-all">Learn More</button> */}
                     </div>
                 </div>
             </div>
-            <div className='px-4 mt-20 md:mt-24 flex justify-center items-center'>
+            <div className='px-4 mt-16 md:mt-24 flex justify-center items-center'>
                 <img src={HomeImg} className='rounded-lg w-full ease-in-out md:w-[66%] h-auto md:h-[600px] border-4 border-secondary transition-all' alt="Coding Image" />
             </div>
         </div>
